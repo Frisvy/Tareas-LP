788585d95e182167d7c48c02743827d7069308c1
Frisvy <gordenes@usm.cl>
2025-08-10 23:40:05 -0400

    pipipi

--- Juez.py
+++ Juez.py
@@ -1,4 +1,23 @@
 import re
 
 archivo = open("estrofas.txt", "r")
-print(archivo.read())
+contador = 1
+# poner el ^ dentro de [] niega todo lo que esta dentro del set
+
+simbolos_validos = "[a-zA-ZñÑ0-9?¿¡!,.;'-():\"]"
+digitos = "[0-9]"
+vocal = "(AEIOU|aeiou)"
+vocal_acentuada = "(ÁÉÍÓÚ|áéíóú)"
+letras = "[A-Za-zÑñ]"
+string = "[]"
+
+"""for linea in archivo:
+    #if not re.search(simbolos_validos, linea):
+    contador + 1
+    if"""
+"""
+x = input("pone un correo wom: ")
+patern = "[A-Za-z0-9]+@[a-z]+\.(com|.cl)"
+if re.search(patern, x):
+    print ("valida la wea")
+"""
