7993e37ff3d2f9c7d450172b8ec7b70aa7deed37
Frisvy <gordenes@usm.cl>
2025-08-16 19:01:42 -0400

    estoy chato loco XD

--- Juez.py
+++ Juez.py
@@ -1,17 +1,19 @@
 import re
+archivo = open("estrofas.txt", "r", encoding="utf-8")
+# poner el ^ dentro de [] niega todo lo que esta dentro del set, se supone...
+#LA PALABRA BONUS DEBE HACER RIMA
+# EBNF -------------------------------------------------------------------------------------
+espacio = r"\s"   # se utiliza para buscar los saltos de linea despues de cada estrofa
+caracteres_permitidos = r"[¿?¡!,.;'():\"-]" 
+digitos = r"[0-9]"
+vocal = r"[AEIOUaeiou]"
+vocal_acentuada = r"[ÁÉÍÓÚáéíóú]"
+letras = r"[A-Za-zÑñ]"
+string = rf"(?:{vocal}|{vocal_acentuada}|{letras})"
+palabra = rf"(?:{string}|{caracteres_permitidos}|{digitos}|{espacio})+"
+#---------------------------------------------------------------------------------------------
 
-archivo = open("estrofas.txt", "r")
-contador = 1
-# poner el ^ dentro de [] niega todo lo que esta dentro del set
-
-simbolos_invalidos = "[^a-zA-ZñÑ0-9¿?¡!,\\.\-;'-():\"\s]"#corregir, no reconoce bien los ¿
-simbolos_validos = "[a-zA-ZñÑ0-9?¿¡!,.-\;'-():\"]"
-digitos = "[0-9]"
-vocal = "(AEIOU|aeiou)"
-vocal_acentuada = "(ÁÉÍÓÚ|áéíóú)"
-letras = "[A-Za-zÑñ]"
-string = "[]"
-espacio = "^\s$"
+tipo_rima = ["consonante", "asonante", "gemela", ]
 
 def estrofas_validas(archivo):
     lista_versos = []
@@ -22,40 +24,73 @@ def estrofas_validas(archivo):
         if ignorar_bonus:
             ignorar_bonus = False
             continue
-        elif not re.match(espacio, linea):
+        elif not re.match(espacio, linea): #si la linea tiene texto, se añade a la lista de versos
             lista_versos.append(linea)
         else:
             if len(lista_versos) == 4:
                 for verso in lista_versos:
-                    coso = re.compile(r"[a-zA-ZáéíóúÁÉÍÓÚñÑ¿?!\",.;:'-]+")
-                    if coso.findall(verso):
+                    if re.fullmatch(palabra, verso):
                         continue
                     else:
                         Estrofa = False
                         lista_versos = []
                         break
-
-                    """if re.search(simbolos_invalidos, verso):
-                        Estrofa = False
-                        lista_versos = []
-                        break"""
+        
                 lista_estrofas.append(Estrofa)
+                Estrofa = True
             else:
                 lista_estrofas.append(False)
-            lista_versos = []
-#elementos de la ultima estrofa 
+            lista_versos = []            
+# como no hay salto de linea despues de la ultima estrofa, no se revisa en el ciclo de arriba.
+
     if len(lista_versos) > 0:
         if len(lista_versos) == 4:
             for verso in lista_versos:
-                if re.search(simbolos_invalidos, verso):
+                if re.fullmatch(palabra, verso):
+                    continue
+                else:
                     Estrofa = False
                     lista_versos = []
-                    break 
+                    break
+                
             lista_estrofas.append(Estrofa)
         else:
-            lista_estrofas.append(False)       
+            lista_estrofas.append(False)
+           
             
-    lista_estrofas.pop(0)
+    lista_estrofas.pop(0) # elimina la linea de bonus de la lista para que hayan solo estrofas
     return lista_estrofas
 
 print(estrofas_validas(archivo))
+
+def Estrofas(archivo):
+    lista_estrofas_validas = estrofas_validas(open("estrofas.txt", "r", encoding="utf-8")) 
+    estrofa_actual = 0
+    lista_estrofa_verso = [] #lista con el contenido de todos los versos
+    bonus = True
+    salta_bonus = 0
+    for cosa in lista_estrofas_validas:
+        lista_estrofa_verso.append([]) # lista de listas donde cada elemento sera una estrofa con todos sus versos
+
+    for linea in archivo:
+        if bonus:
+            salta_bonus +=1
+            if salta_bonus == 2:
+                bonus = False
+
+        else:
+            if lista_estrofas_validas[estrofa_actual]:
+                if len(lista_estrofa_verso[estrofa_actual]) < 4:
+                    lista_estrofa_verso[estrofa_actual].append(linea.strip())
+                else:
+                    estrofa_actual += 1
+            else:
+                lista_estrofa_verso[estrofa_actual].append(linea.strip())
+                if lista_estrofa_verso[estrofa_actual][-1] == "":
+                        estrofa_actual += 1
+
+    return lista_estrofa_verso
+
+print(Estrofas(open("estrofas.txt", "r", encoding="utf-8")))
+
+#def consonantes()
---
+++ README.txt
@@ -0,0 +1,4 @@
+Nombre: Gabriel Ordenes
+Rol: 202473521-7
+
+La funcion estrofas_validas lee el archivo considerando que siempre va a haber una linea bonus independientemente de su contenido, por lo cual si no se pone terminaria por eliminar una estrofa en la lista que retorna, ademas se entiende que siempre habra un salto de linea que separe una estrofa de otra, y que no existira nunca un linea vacia seguida de otra. (considere que cada archivo estrofas.txt tendria ese formato)
