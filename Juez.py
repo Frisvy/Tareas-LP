c0043bb1c78a16daba8a4c6bd61fabe276986a7c
Frisvy <gordenes@usm.cl>
2025-08-20 20:44:34 -0400

    terminaada mi washooooo

--- Juez.py
+++ Juez.py
@@ -157,23 +157,40 @@ def final_versos(estrofa): #pasarle un indice de lista_estrofa_verso, para resca
 ***
 Nombre: quitar_tildes_signos
 ***
-Parametro 1: ultima_palabra_versos
+Parametro 1: Lista de strings
 ***
 Tipo de retorno: Lista de strings
 ***
-Descripcion: Lee la primera linea de estrofas.txt y retonar una lista con las palabras Bonus
+Descripcion: Recibe una lista que contenga la ultima palabra de los versos de una estrofa
+             y retorna las mismas palabras pero sin signos como ".,-" y sin tildes, ademas
+             pasa todas las palabras a minusculas
 """
 def quitar_tildes_y_signos(ultima_palabra_versos):#quita tildes y signos de cualquier tipo
     palabras_sin_tildes = []
     vocales_a_reemplazar = {"á":"a", "é":"e", "í":"i", "ó": "o", "ú": "u"}
     for palabra in ultima_palabra_versos:
         palabra = re.sub(caracteres_permitidos, "", palabra) #para eliminar caracteres que puedan interferir con la rima
+        palabra = re.sub(digitos,"",palabra) #elimina los numeros
         for tilde, letra in vocales_a_reemplazar.items():
             palabra = re.sub(tilde,letra,palabra)
         palabras_sin_tildes.append(palabra)
 
     return palabras_sin_tildes
 
+"""
+***
+Nombre: Lista_definitiva
+***
+Parametro 1: Lista de strings con la ultima palabra de cada verso
+Parametro 2: Lista boleanos con las estrofas validas
+***
+Tipo de retorno: Lista de strings
+***
+Descripcion: Recibe una lista que contenga la ultima palabra de los versos de una estrofa
+             y una lista de booleanos con la cantidad de estrofas validas o invalidas (sacada de la funcion lista_estrofas_validas)
+             la funcion retorna una lista con las 6 tuplas posibles de un verso valido,
+             en caso de que la estrofa sea invalida, esta lista tendra una tupla de (False, False)
+"""
 def Lista_definitiva(lista_estrofas_casi_lista, estrofas_validas):
     lista_retorno = []
     for estrofa, valida in zip(lista_estrofas_casi_lista, estrofas_validas): 
@@ -190,6 +207,21 @@ def Lista_definitiva(lista_estrofas_casi_lista, estrofas_validas):
 
     return lista_retorno
 
+"""
+***
+Nombre: largo_rima_consonante
+***
+Parametro 1: string de palabra1 a comparar
+Parametro 2: string de palabra2 a comparar
+***
+Tipo de retorno: int del largo de la rima consonante encontrada
+***
+Descripcion: Dadas 2 palabras se calcula el largo del sufijo que tienen en comun (rima), y se retorna.
+             para esto se crea un patron con las letras de la palabra mas grande, despues de eso se va
+             comparando caracter a caracter desde el final hacia el inicio de la palabra. El ciclo
+             se corta una vez se encuentra una palbra distinta, acto seguido se retorna el largo en comun de la rima
+"""
+
 def largo_rima_consonante(palabra1,palabra2): #permite saber cuanto puntaje hay que otorgar con la rima consonante
     largo_comun = 0
     max_largo = min(len(palabra1), len(palabra2)) #largo max teorico que podria tener el sufijo en comun
@@ -197,7 +229,7 @@ def largo_rima_consonante(palabra1,palabra2): #permite saber cuanto puntaje hay 
         largo_prueba = largo_comun + 1
         sufijo1 = palabra1[-largo_prueba:]
         sufijo2 = palabra2[-largo_prueba:]
-        patron_sufijo = rf"{re.escape(sufijo1)}$" # creamos un patron con el prefijo que queremos comparar, para ver si son iguales entre las 2 palabras
+        patron_sufijo = rf"{sufijo1}$" # creamos un patron con el prefijo que queremos comparar, para ver si son iguales entre las 2 palabras
         if re.search(patron_sufijo, palabra2):
             largo_comun = largo_prueba 
         else:
@@ -206,8 +238,35 @@ def largo_rima_consonante(palabra1,palabra2): #permite saber cuanto puntaje hay 
 
 #print(largo_rima_consonante("inicial","superficial"))
 
+"""
+***
+Nombre: vocales_rima
+***
+Parametro 1: string de palabra a modificar
+***
+Tipo de retorno: string de vocales
+***
+Descripcion: Dado el string de una palabra, se usa el findall para encontrar todas las coincidencias de vocales
+             en una palabra, para acto seguido unirlas todas en un unico strig de vocales. Es decir
+             se retorna la palabra unicamente con sus vocales
+"""
 def vocales_rima(palabra): #retorna unicamente las vocales de una palabra
     return "".join(re.findall(vocal,palabra))
+
+"""
+***
+Nombre: largo_rima_asonante
+***
+Parametro 1: string de palabra1 a comparar
+Parametro 2: string de palabra2 a comparar
+***
+Tipo de retorno: int correspondiente del largo de la rima
+***
+Descripcion: Dadas 2 palabras, se extraen todas sus consonantes con la funcion vocales_rima
+             para dejar unicamente las vocales, acto seguido se van comparando las vocales desde el
+             final hacia el principio para determinar el largo de la rima asonante. La logica de esta
+             funcion es practicamente la misma que la de la funcion largo_rima_consonante
+"""
 
 def largo_rima_asonante(palabra1,palabra2): #la estructura es practicamente la misma de la funcion de la rima asonante
     vocalesp1 = vocales_rima(palabra1)
@@ -218,13 +277,27 @@ def largo_rima_asonante(palabra1,palabra2): #la estructura es practicamente la m
         largo_prueba = largo_comun + 1
         vocales_finales1 = vocalesp1[-largo_prueba:]
         vocales_finales2 = vocalesp2[-largo_prueba:]
-        patron_vocales1 = rf"{re.escape(vocales_finales1)}$"
+        patron_vocales1 = rf"{vocales_finales1}$"
         if re.search(patron_vocales1,   vocalesp2):
             largo_comun = largo_prueba
         else:
             break
     return largo_comun
 
+"""
+***
+Nombre: rimas
+***
+Parametro 1: Lista de estrofas con sus tuplas correspondientes
+Parametro 2: Lista de palabras bonus
+***
+Tipo de retorno 1: Lista con los tipos de rima encontrados por estrofa
+Tipo de retorno 2: Lista con los puntajes brutos de cada tupla
+***
+Descripcion: Dadas las listas, se calculan las rimas encontradas en cada estrofa.
+             Ademas de esto se llena una lista con el puntaje bruto de cada tupla
+***
+"""
 #print(largo_rima_asonante("malambo","mangos"))
 # Lista_final_estrofas = [[()]]
 def rimas(lista_final_estrofas,lista_bonus): #pasar bonus limpio
@@ -237,6 +310,7 @@ def rimas(lista_final_estrofas,lista_bonus): #pasar bonus limpio
         lista_rimas.append([])
         puntaje.append([])
         Bonus = True
+        Sin_rima = True
         for lista_tupla in estrofa:
             if lista_tupla[0] != False:
                 asonante = largo_rima_asonante(lista_tupla[0],lista_tupla[1])
@@ -259,7 +333,7 @@ def rimas(lista_final_estrofas,lista_bonus): #pasar bonus limpio
                             puntaje[numero_de_estrofa].append(2)
                             lista_rimas[numero_de_estrofa].append("Bonus")
                             Bonus = False
-                elif (consonante == 3 or consonante == 4) and (asonante < 3): #el and esta para que se priorize la que de mas puntaje
+                elif (consonante == 3 or consonante == 4) and (asonante < 3): #el and esta para que se priorize la rima que de mas puntaje, en caso de revisar una estrofa con solo rimas consonantes basta borrar el and para que se les de prioridad a estas y se calcule solo ese puntaje :3
                     puntaje[numero_de_estrofa].append(5)
                     lista_rimas[numero_de_estrofa].append("consonante")
                     if Bonus:
@@ -311,6 +385,21 @@ def rimas(lista_final_estrofas,lista_bonus): #pasar bonus limpio
         numero_de_estrofa += 1
     return lista_rimas, puntaje #puntaje bruto
 
+"""
+***
+Nombre: lista_rimas_sin_repeticion
+***
+Parametro 1: Lista de rimas encontradas por estrofa
+***
+Tipo de retorno 1: Lista con los tipos de rima encontrados por estrofa
+***
+Descripcion: Recibe la lista de rimas encontradas por tupla. Con esta lista
+             se borran todos los elementos repetidos para que a la hora de escribir
+             el archivo sea mas facil, y ademas no salgan elementos repetidos, por ejemplo
+             Rimas: Asonante Asonante Asonante. En vez de eso solo saldra Asonante una vez
+***
+"""
+
 def lista_rimas_sin_repeticion(lista_rimas):
     rimas_estrofa_sin_repetidos = []
     for estrofa in lista_rimas:
@@ -322,6 +411,22 @@ def lista_rimas_sin_repeticion(lista_rimas):
 
     return rimas_estrofa_sin_repetidos
 
+"""
+***
+Nombre: decision
+***
+Parametro 1: Lista de rimas por estrofa
+Parametro 2: Lista de puntajes brutos
+***
+Tipo de retorno 1: no retorna
+***
+Descripcion: Recibe la lista de Rimas por estrofa y la lista de puntajes brutos por estrofa para escribir
+             el archivo "decision.txt".para eso la funcion primero verifica si hay algun tipo de rima en la 
+             lista de rimas, si no encuentra ninguna rima implica que la estrofa era invalida, por lo cual escribe 
+             Estrofa i: Invalida. Si se encuentra la rima, se escribe el numero de estrofa, con su puntaje correspondiente
+             y las rimas encontradas 
+***
+"""
 def decision( lista_rimas, lista_puntajes_brutos):
     with open("decision.txt", "w", encoding="utf-8") as decision:
         i = 1
@@ -329,17 +434,12 @@ def decision( lista_rimas, lista_puntajes_brutos):
             if not estrofa_r:
                 decision.write(f"Estrofa {i}: Invalida\n")
             else:
-                puntaje = round((sum(estrofa_p)/5),1)
+                puntaje = round((sum(estrofa_p)/5),1) # se calcula el puntaje en escala 0/10 redondeado a 1 decimal
+                if puntaje < 0 : # en caso de que el puntaje sea negativo se deja en 0
+                    puntaje = 0
                 decision.write(f"Estrofa {i}: {puntaje}\n")
                 decision.write(f"Rimas : {', '.join(estrofa_r)}\n")
             i += 1
-            
-
-    
-
-
-
-
 
 #---------------------------------------MAIN------------------------------------------------------
 """ Se llaman a las funciones para armar el archivo, el for termina de crear y armar las listas 
@@ -358,14 +458,18 @@ Estrofas_versos = Estrofas(open("estrofas.txt", "r", encoding="utf-8")) #se crea
 Lista_estrofas_casi_lista = [] 
 for Estrofa in Estrofas_versos: #ciclo para limpiar las palabras de la lista Estrofas_versos
     Estrofa_con_palabras_finales = quitar_tildes_y_signos(final_versos(Estrofa)) #los versos pasan a tener solo las palabras finales y a tener las palabras en minusculas sin simbolos permitidos
-    Lista_estrofas_casi_lista.append(Estrofa_con_palabras_finales) #queda con las palabras finales sin simbolos raros ni nada
-#print(Lista_estrofas_casi_lista)
+    Lista_estrofas_casi_lista.append(Estrofa_con_palabras_finales)  #queda con las palabras finales sin simbolos raros ni nada
+
 lista_final_estrofas = Lista_definitiva(Lista_estrofas_casi_lista, Estrofas_validas) #lista con todas las tuplas por estrofa, si la estrofa es invalida en vez de tuplas con palabras  hay un (False, False) ademas de eso las parejas tienen todos los simbolos limpios, no considera numeros la limpieza
-#print(lista_final_estrofas,palabras_bonus)
-#print(rimas(lista_final_estrofas, palabras_bonus))
+
 lista_rimas, lista_puntajes_brutos = rimas(lista_final_estrofas, palabras_bonus)
 lista_rimas = lista_rimas_sin_repeticion(lista_rimas)
-#print(lista_rimas)
 decision(lista_rimas,lista_puntajes_brutos)
+
+#En caso de querer ver el calculo de rimas por estrofa y pareja utilizar este print: print(rimas(lista_final_estrofas, palabras_bonus))
+
+#print(Lista_estrofas_casi_lista)
+#print(lista_final_estrofas,palabras_bonus)
+#print(rimas(lista_final_estrofas, palabras_bonus))
 
 
--- README.txt
+++ README.txt
@@ -2,4 +2,6 @@ Nombre: Gabriel Ordenes
 Rol: 202473521-7
 
 La funcion estrofas_validas lee el archivo considerando que siempre va a haber una linea bonus independientemente de su contenido, por lo cual si no se pone terminaria por eliminar una estrofa en la lista que retorna, ademas se entiende que siempre habra un salto de linea que separe una estrofa de otra, y que no existira nunca un linea vacia seguida de otra. (considere que cada archivo estrofas.txt tendria ese formato)
-En cuanto a las rimas se si bien el codigo identificara correctamente los simbolos permitidos, se asume que la ultima linea de cualquier verso no sera nunca un numero, si no que sera una palabra. (Esto para la correcta identificacion de rimas)
+En cuanto a las rimas si bien el codigo identificara correctamente los simbolos permitidos, se asume que la ultima linea de cualquier verso sera una palabra y no un numero escrito como "1" (palabras reales, no del estilo "klanjdsldalkd" o "holaaaaaaaaaaaaaa", o "hol1", etc). (Esto para la correcta identificacion de rimas, ya que los numeros que se encuentren se reemplazaran con un "" directamente)
+El codigo aplicara la deteccion de todos los tipos de rimas al instante
+El codigo para funcionar necesita tener en la misma carpeta el archivo de estrofas.txt, eso deberia bastar
